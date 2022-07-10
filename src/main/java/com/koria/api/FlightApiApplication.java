c50787077af0535e5ebf375b87d098b7d4a2d0ad
koria <lanskeit6@gmail.com>
2022-07-10 21:15:19 +0100

    Commit sur le deployement

--- src/main/java/com/koria/api/FlightApiApplication.java
+++ src/main/java/com/koria/api/FlightApiApplication.java
@@ -2,12 +2,20 @@ package com.koria.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlightApiApplication {
+public class FlightApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightApiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(FlightApiApplication.class);
 	}
 
 }
